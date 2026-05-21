63aa620ef8f7d59d22caabc0cfe8d4a429010a69
Maluu26 <maria.lfr.santos@edu.ufes.br>
2026-05-21 14:57:29 -0300

    doing de

--- differencial_evolution.py
+++ differencial_evolution.py
@@ -11,11 +11,41 @@ def fitnessFunction(x):
 
     return result + penalidade 
 
-def mutation():     
+def selectInd(pop):
+    return pop[np.random.randint(0,100)]
+
+def mutation(pop, fitness, F):     
+    
+    i1 = selectInd(pop)
+    i2 = selectInd(pop)
+    i3 = selectInd(pop)
+    return (i1 +  F(i2 - i3))
+
+def crossOver(iPop, mutPop, cr):
+    newPop = []
+    for i in range(len(iPop)):
+        indM = selectInd(mutPop) 
+        indO = selectInd(mutPop) 
+        for j in range(N):
+            if np.random.rand()<cr:  
+                newPop[i][j] = indM[j] 
+            else:
+                newPop[i][j] = indO[j] 
+
+    return newPop
+
+def selection(initialPop, crossPop):
     
 def differentialEvolution(F, cr):
     
     initialPop = np.random.randint(low = -32, high=32,size=(100,30))
+    fitnessPop = np.array([fitnessFunction(ind) for ind in initialPop])
+    middlePop = [mutation(initialPop,fitnessPop,F) for i in range (100)]
+    crossPop = crossOver(initialPop, middlePop,cr)
+    newPop = selection()
+    historico = list()
+    #np.clip para mutação
+
    
 def main():
     differentialEvolution(0.5, 0.3)
