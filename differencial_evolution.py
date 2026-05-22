d4cbf82bccd2cae505178c22b08b12d6769770b1
Maria Luiza Ferreira Reis Santos <malu@Marias-MacBook-Air.local>
2026-05-21 21:31:43 -0300

    adujsting de

--- differencial_evolution.py
+++ differencial_evolution.py
@@ -1,6 +1,7 @@
 import numpy as np
 N = 30
-
+popSize = 50
+generations = 1000
 def fitnessFunction(x):
     
     somatorio1 = 1/N * np.sum(x**2)
@@ -12,17 +13,18 @@ def fitnessFunction(x):
     return result + penalidade 
 
 def selectInd(pop):
-    return pop[np.random.randint(0,100)]
+    return pop[np.random.randint(0,popSize)]
 
 def mutation(pop, fitness, F):     
     
     i1 = selectInd(pop)
     i2 = selectInd(pop)
     i3 = selectInd(pop)
-    return (i1 +  F(i2 - i3))
+    mut = i1 +  F*(i2 - i3)
+    return (np.clip(mut, -32, 32))
 
 def crossOver(iPop, mutPop, cr):
-    newPop = []
+    newPop = np.zeros((popSize, 30))
     for i in range(len(iPop)):
         indM = selectInd(mutPop) 
         indO = selectInd(mutPop) 
@@ -35,20 +37,31 @@ def crossOver(iPop, mutPop, cr):
     return newPop
 
 def selection(initialPop, crossPop):
-    
+    newPop = np.zeros((popSize,30))
+    for i in range(len(initialPop)):
+        f1 = fitnessFunction(initialPop[i])
+        f2 = fitnessFunction(crossPop[i])
+
+        if(f1<f2): newPop[i] = initialPop[i]
+        else: newPop[i] = crossPop[i]
+    return newPop        
+
 def differentialEvolution(F, cr):
     
-    initialPop = np.random.randint(low = -32, high=32,size=(100,30))
+    initialPop = np.random.randint(low = -32, high=32,size=(popSize,30))
+    for i in range (generations):
+        fitnessPop = np.array([fitnessFunction(ind) for ind in initialPop])
+        middlePop = [mutation(initialPop,fitnessPop,F) for i in range (popSize)]
+        crossPop = crossOver(initialPop, middlePop,cr)
+        newPop = selection(initialPop, crossPop)
+        initialPop = newPop
     fitnessPop = np.array([fitnessFunction(ind) for ind in initialPop])
-    middlePop = [mutation(initialPop,fitnessPop,F) for i in range (100)]
-    crossPop = crossOver(initialPop, middlePop,cr)
-    newPop = selection()
-    historico = list()
-    #np.clip para mutação
+    return np.min(fitnessPop)
+    
 
    
 def main():
-    differentialEvolution(0.5, 0.3)
-
+    de = differentialEvolution(0.5, 0.3)
+    print(de)
 if __name__ == '__main__':
     main()
